a3b4ef4b1923d03f707500df0a536e52133b567d
Samir IZZA <samirmember@gmail.com>
2026-01-17 01:34:34 +0100

    [First commit] Symfony and Angular on Docker with WSL2

---
+++ server/src/Command/CreateAdminCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-admin',
+    description: 'Crée un utilisateur admin (ROLE_ADMIN).'
+)]
+class CreateAdminCommand extends Command
+{
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+        private readonly UserPasswordHasherInterface $hasher
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'Email')
+            ->addArgument('password', InputArgument::REQUIRED, 'Mot de passe');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $email = (string) $input->getArgument('email');
+        $password = (string) $input->getArgument('password');
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->hasher->hashPassword($user, $password));
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $output->writeln('Admin créé: '.$email);
+        return Command::SUCCESS;
+    }
+}
