f7b34783af0861c97ba00b3f29e5521f4041d3b9
Samir IZZA <samirmember@gmail.com>
2026-01-18 16:42:09 +0100

    A lot of changes

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '',
+    loadComponent: () => import('./page/home/home.component.js').then(m => m.HomeComponent)
+},
+{
+    path: 'equipes/algerie/seniors',
+    loadComponent: () => import('./page/en-senior/en-senior.component.js').then(m => m.EnSeniorComponent),
+  },
+  { path: '**', redirectTo: '' },
+];
---
+++ front/src/app/page/en-senior/en-senior.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-en-senior',
+  imports: [],
+  templateUrl: './en-senior.component.html',
+  styleUrl: './en-senior.component.scss',
+})
+export class EnSeniorComponent {
+
+}
