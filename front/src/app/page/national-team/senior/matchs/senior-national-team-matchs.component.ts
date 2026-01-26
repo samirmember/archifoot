e0c976b56630466d7510e28c5ce01dbe4c004166
Samir IZZA <samirmember@gmail.com>
2026-01-26 15:36:50 +0100

    [WIP] Matchs page of Senior National Team

--- front/src/app/page/national-team/senior/matchs/senior-national-team-matchs.component.ts
+++ front/src/app/page/national-team/senior/matchs/senior-national-team-matchs.component.ts
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './senior-national-team-matchs.component.scss',
 })
 export class SeniorNationalTeamMatchsComponent {
+  countries = ['France', 'Germany', 'Italy', 'Spain', 'Portugal'];
+  // years = Array.from({ length: 30 }, (_, i) => new Date().getFullYear() - i);
 
+  years = this.populateYears();
+
+  private populateYears(): number[] {
+    const startYear = 1962;
+    const currentYear = new Date().getFullYear();
+    const years: number[] = [];
+
+    for (let year = startYear; year <= currentYear; year++) {
+      years.push(year);
+    }
+
+    return years;
+  }
 }
