e6bdcdd8abf95d6f7dc337a0ff4472bb1fd76ce5
Samir IZZA <samirmember@gmail.com>
2026-01-31 00:39:22 +0100

    API first route

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   api: {
-    baseUrl: 'http://localhost:8000/api',
-    apiKey: 'dev-api-key-change-me',
+    baseUrl: 'http://localhost:8080/api',
+    apiKey: 'kRaLrpy69t9vzvZ0BW4HB11OYyZXeDlWsdMxlNSi0zgajkKseGXuUhAX9pIsOFbU',
   },
 };
