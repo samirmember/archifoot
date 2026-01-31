40b747d7534771a20999f9593f1295458ed37487
Samir IZZA <samirmember@gmail.com>
2026-01-31 16:46:46 +0100

    Remove GLightbox

--- front/public/assets/sandbox/js/theme.js
+++ front/public/assets/sandbox/js/theme.js
@@ -21,7 +21,6 @@ var theme = {
     // theme.rellax();
     // theme.scrollCue();
     // theme.swiperSlider();
-    // theme.lightbox();
     // theme.plyr();
     // theme.progressBar();
     // theme.loader();
@@ -515,47 +514,6 @@ var theme = {
         },
       });
     }
-  },
-  /**
-   * GLightbox
-   * Enables lightbox functionality
-   * Requires assets/js/vendor/glightbox.js
-   */
-  lightbox: () => {
-    const lightbox = GLightbox({
-      selector: '*[data-glightbox]',
-      touchNavigation: true,
-      loop: false,
-      zoomable: false,
-      autoplayVideos: true,
-      moreLength: 0,
-      slideExtraAttributes: {
-        poster: '',
-      },
-      plyr: {
-        css: '',
-        js: '',
-        config: {
-          ratio: '',
-          fullscreen: {
-            enabled: false,
-            iosNative: false,
-          },
-          youtube: {
-            noCookie: true,
-            rel: 0,
-            showinfo: 0,
-            iv_load_policy: 3,
-          },
-          vimeo: {
-            byline: false,
-            portrait: false,
-            title: false,
-            transparent: false,
-          },
-        },
-      },
-    });
   },
   /**
    * Plyr
