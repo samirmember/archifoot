9a822fc9d2dfcabe3ebb70bffdfbd5b4bedd6ebe
Samir IZZA <samirmember@gmail.com>
2026-02-06 19:07:41 +0100

    Fix Team entity Doctrine classes import

--- server/src/Entity/Team.php
+++ server/src/Entity/Team.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
