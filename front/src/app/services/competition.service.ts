6afcaac0df91cd40db16d76fb05b5e7659475b5c
Samir IZZA <samirmember@gmail.com>
2026-02-06 11:40:40 +0100

    feat(front): add competition list retrieval in CompetitionService

--- front/src/app/services/competition.service.ts
+++ front/src/app/services/competition.service.ts
@@ -1,6 +1,29 @@
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { ApiClientService } from '../../shared/api/api-client.service';
+
+export interface Competition {
+  id: number;
+  name: string | null;
+  category?: string | null;
+  [key: string]: unknown;
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class CompetitionService {}
+export class CompetitionService {
+  constructor(private readonly apiClient: ApiClientService) {}
+
+  public getCompetitios(): Observable<Competition[]> {
+    return this.apiClient.getCollection<Competition>('competitions', { pagination: 'false' }).pipe(
+      map((response) => {
+        if (Array.isArray(response)) {
+          return response as Competition[];
+        }
+
+        return Array.isArray(response['hydra:member']) ? response['hydra:member'] : [];
+      }),
+    );
+  }
+}
