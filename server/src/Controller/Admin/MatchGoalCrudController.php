f6075d0c8cb62351241a51f0a6bf1b216fe443d9
Samir IZZA <samirmember@gmail.com>
2026-02-08 19:14:18 +0100

    Configure EasyAdmin dropdown fields for goals and stadium city

--- server/src/Controller/Admin/MatchGoalCrudController.php
+++ server/src/Controller/Admin/MatchGoalCrudController.php
@@ -4,11 +4,32 @@ namespace App\Controller\Admin;
 
 use App\Entity\MatchGoal;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class MatchGoalCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return MatchGoal::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            AssociationField::new('fixture'),
+            AssociationField::new('team'),
+            AssociationField::new('scorer'),
+            TextField::new('scorerText'),
+            TextField::new('minute'),
+            ChoiceField::new('goalType', 'goal_type')->setChoices([
+                'Normal' => 'normal',
+                'Pénalty' => 'penalty',
+                'But CSC' => 'own_goal',
+            ]),
+        ];
     }
 }
--- server/src/Controller/Admin/StadiumCrudController.php
+++ server/src/Controller/Admin/StadiumCrudController.php
@@ -4,11 +4,26 @@ namespace App\Controller\Admin;
 
 use App\Entity\Stadium;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class StadiumCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Stadium::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name'),
+            AssociationField::new('city', 'city_id'),
+            AssociationField::new('country', 'country_id'),
+            IntegerField::new('capacity'),
+        ];
     }
 }
