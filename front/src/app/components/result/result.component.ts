329bdc785ebc4c580b3aafd75ca25aeb17308275
Samir IZZA <samirmember@gmail.com>
2026-02-09 10:52:49 +0100

    [Front] Remove bootstrap import and use

--- front/src/app/components/result/result.component.ts
+++ front/src/app/components/result/result.component.ts
@@ -1,8 +1,7 @@
 import { DatePipe } from '@angular/common';
-import { AfterViewInit, Component, computed, input, ElementRef } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { FlagComponent } from 'src/app/layouts/flag/flag.component';
 import { MatchResult } from 'src/app/services/result.service';
-import { Tooltip } from 'bootstrap';
 import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
@@ -11,29 +10,8 @@ import { TooltipModule } from 'primeng/tooltip';
   templateUrl: './result.component.html',
   styleUrl: './result.component.scss',
 })
-export class ResultComponent implements AfterViewInit {
+export class ResultComponent {
   result = input.required<MatchResult>();
   readonly iso2A = computed(() => this.result().countryCodeA?.toLowerCase() ?? '');
   readonly iso2B = computed(() => this.result().countryCodeB?.toLowerCase() ?? '');
-
-  constructor(private readonly elRef: ElementRef<HTMLElement>) {}
-
-  ngAfterViewInit(): void {
-    this.initTooltip();
-  }
-
-  private initTooltip(): void {
-    const root = this.elRef.nativeElement;
-
-    root.querySelectorAll<HTMLElement>('[data-bs-toggle="tooltip"]').forEach((el) => {
-      Tooltip.getOrCreateInstance(el, {
-        container: 'body',
-        boundary: 'window',
-        placement: 'top',
-        popperConfig: {
-          strategy: 'fixed',
-        },
-      });
-    });
-  }
 }
